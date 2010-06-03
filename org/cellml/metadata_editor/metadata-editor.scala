8b042e1977bc67df738f8a084b6546c5e52c511d
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2010-06-03 16:42:28 +1200

    We now have a a multipart editor working.

--- org/cellml/metadata_editor/metadata-editor.scala
+++ org/cellml/metadata_editor/metadata-editor.scala
@@ -7,6 +7,12 @@ import com.hp.hpl.jena.rdf.arp._
 import com.hp.hpl.jena.rdf.model.{Model => Model, Alt => Alt, Bag => Bag, Seq => JSeq, Container => JContainer, RDFNode => RDFNode, ModelFactory => ModelFactory, Property => Property, Statement => Statement, Resource => Resource}
 
 object MetadataEditor extends SimpleSwingApplication {
+  type propertyable = { def getProperty(a: Property): Statement; def addProperty(a: Property, b: String): Resource }
+  class stmt2prop(stmt: Statement) {
+    def getProperty(a: Property): Statement = stmt.getProperty(a)
+    def addProperty(a: Property, b: String) = stmt.getResource().addProperty(a, b)
+  }
+  type propertyish = { def getProperty(a: Property): Statement }
   implicit def container2seq(thing: JContainer): Seq[RDFNode] = {
     var f: Seq[RDFNode] = Seq()
     var iter = thing.iterator
@@ -14,12 +20,12 @@ object MetadataEditor extends SimpleSwingApplication {
       f = f :+ iter.next()
     f
   }
-
+  implicit def statement2propertyable(stmt: Statement): propertyable = new stmt2prop(stmt)
   case class editor(schema: String, value: String) extends FlowPanel {
     contents += new Label(schema)
     contents += new TextField { text = value }
   }
-  case class ResourceEditor(root: Resource, predicate: Property) extends FlowPanel {
+  case class ResourceEditor(root: propertyable, predicate: Property) extends FlowPanel {
     def get: String = root.getProperty(predicate).getString()
     def set(value: String): Unit = root.addProperty(predicate, value)
 
@@ -30,11 +36,14 @@ object MetadataEditor extends SimpleSwingApplication {
       case EditDone(inneredit) => set(inneredit.text)
     }
   }
-  case class BagEditor(root: Bag, builder: Resource => FlowPanel) extends FlowPanel {
+  case class BagEditor(root: Bag, builder: propertyable => FlowPanel) extends FlowPanel {
     root.foreach(a => contents += builder(a.as(classOf[com.hp.hpl.jena.rdf.model.Resource])))
   }
-  case class CompoundEditor(root: Resource, builder: Seq[Resource => FlowPanel]) extends FlowPanel {
+  case class CompoundEditor(root: propertyable, builder: Seq[propertyable => FlowPanel]) extends FlowPanel {
     builder.foreach(a => contents += a(root))
+  }
+  def labeledtext(label: String, predicate: Property)(root: propertyable): FlowPanel = {
+    new FlowPanel(new Label(label), ResourceEditor(root, predicate))
   }
   def editors(file: File): Panel = {
     var fis = new FileInputStream(file)
@@ -48,9 +57,18 @@ object MetadataEditor extends SimpleSwingApplication {
     //arw.setProperty("relativeURIs", "same-document, absolute, relative, parent")
     //arw.write(m, System.out, cmetans)
     val aboutModel = m.getResource("")
-    
-    val controls: List[editor] = types.map(a => editor(a._1, a._2("getter")(m)))
-    new FlowPanel(controls: _*)
+    val vcn = m.createProperty("http://www.w3.org/2001/vcard-rdf/3.0#N")
+    val vcg = m.createProperty("http://www.w3.org/2001/vcard-rdf/3.0#Given")
+    val vcf = m.createProperty("http://www.w3.org/2001/vcard-rdf/3.0#Family")
+    val dcc = m.createProperty("http://purl.org/dc/elements/1.1/creator")
+    val controls = CompoundEditor(aboutModel, Seq(a => CompoundEditor(a.getProperty(dcc), Seq(
+      b => CompoundEditor(b.getProperty(vcn), Seq(
+        labeledtext("Given Name: ", vcg), labeledtext("Family Name: ", vcf)
+      ))
+    ))))
+    controls
+    //val controls: List[editor] = types.map(a => editor(a._1, a._2("getter")(m)))
+    //new FlowPanel(controls: _*)
   }
   def getVCardGivenName(mod: Model, vcard: {def getProperty(a: Property): Statement}): String = {
     vcard.getProperty(mod.createProperty("http://www.w3.org/2001/vcard-rdf/3.0#N"))
