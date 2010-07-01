2e3496d271032ce462683b4197fdda88ebd24365
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2010-07-02 11:43:50 +1200

    Start separation of Jena wrapper code.

---
+++ org/cellml/metadata_editor/jena-wrapper.scala
@@ -0,0 +1,88 @@
+package org.cellml.metadata_editor
+
+import com.hp.hpl.jena.rdf.arp._
+import com.hp.hpl.jena.rdf.model.{Model, Alt, Bag, Seq => JSeq, Container => JContainer, RDFNode, ModelFactory, Property, Statement, Resource}
+
+import scala.swing._
+
+object JenaWrapper {
+  trait stringable { def getString(): String }
+  trait propertyable extends stringable {
+    def getProperty(a: Property): Option[propertyable]
+    def addProperty(a: Property, b: String): Unit 
+    def addProperty(a: Property, b: RDFNode): Unit
+    def hasProperty(a: Property): Boolean
+    def removeAll(a: Property): Unit
+    def getModel(): Option[Model]
+    def canAs[A <: RDFNode](clazz: Class[A]): Boolean
+    def as[A <: RDFNode](clazz: Class[A]): Option[A]
+  }
+  type thingable = Component with propertyable
+  implicit def stmt2saferes(stmt: Statement): propertyable = {
+    if (stmt.getObject().isLiteral())
+      new litResWrapper(stmt.getObject())
+    else
+      new safeResWrapper(stmt.getResource())
+  }
+  implicit def res2prop(res: Resource): propertyable = {
+    if (res.isLiteral())
+      new litResWrapper(res)
+    else
+      new safeResWrapper(res)
+  }
+  class litResWrapper(proxy: RDFNode) extends propertyable {
+    def getProperty(a: Property): Option[propertyable] = None
+    def addProperty(a: Property, b: String) = Unit
+    def addProperty(a: Property, b: RDFNode) = Unit
+    def hasProperty(a: Property) = false
+    def removeAll(a: Property) = Unit
+    def getModel(): Option[Model] = None 
+    def canAs[A <: RDFNode](clazz: Class[A]): Boolean = proxy.canAs(clazz)
+    def as[A <: RDFNode](clazz: Class[A]): Option[A] = {
+      if(canAs(clazz)) {
+        try {
+          Some(proxy.as(clazz))
+        } catch {
+          case e => None
+        }
+      }
+      else
+        None
+    }
+    def getString(): String = proxy.toString()
+  }
+  class safeResWrapper(proxy: Resource) extends propertyable {
+    def getProperty(a: Property): Option[propertyable] = {
+      if (!proxy.hasProperty(a))
+        None
+      else
+        Some(proxy.getProperty(a))
+    }
+    def addProperty(a: Property, b: String) = proxy.addProperty(a, b)
+    def addProperty(a: Property, b: RDFNode) = proxy.addProperty(a, b)
+    def hasProperty(a: Property) = proxy.hasProperty(a)
+    def removeAll(a: Property) = proxy.removeAll(a)
+    def getModel(): Option[Model] = Some(proxy.getModel())
+    def canAs[A <: RDFNode](clazz: Class[A]): Boolean = proxy.canAs(clazz)
+    def as[A <: RDFNode](clazz: Class[A]): Option[A] = {
+      if(canAs(clazz)) {
+        try {
+          Some(proxy.as(clazz))
+        } catch {
+          case e => None
+        }
+      }
+      else
+        None
+    }
+    def getString(): String = proxy.toString()
+  }
+  implicit def container2seq(thing: JContainer): Seq[RDFNode] = {
+    var f: Seq[RDFNode] = Seq()
+    var iter = thing.iterator
+    while(iter.hasNext)
+      f = f :+ iter.next()
+    iter.close()
+    f
+  }
+}
